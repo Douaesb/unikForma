78ddcece33956ed916759d1323fb0968363552fa
Douae sb <douae.sb411@example.com>
2024-10-28 14:55:19 +0100

    UN-16-configure-logger

--- src/main/java/com/unik/unikForma/UnikFormaApplication.java
+++ src/main/java/com/unik/unikForma/UnikFormaApplication.java
@@ -1,13 +1,20 @@
 package com.unik.unikForma;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnikFormaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnikFormaApplication.class, args);
-	}
+	private static final Logger logger = LoggerFactory.getLogger(UnikFormaApplication.class);
 
+	public static void main(String[] args) {
+		// Start the Spring application
+		SpringApplication.run(UnikFormaApplication.class, args);
+
+		// Log a test message
+		logger.info("Test logger");
+	}
 }
