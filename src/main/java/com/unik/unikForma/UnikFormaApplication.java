4deaea865db803f4ce5a01eed72a0ac51e6eef98
Douae sb <douae.sb411@example.com>
2024-10-28 14:33:40 +0100

    UN-15-initalize-spring-boot-project

---
+++ src/main/java/com/unik/unikForma/UnikFormaApplication.java
@@ -0,0 +1,13 @@
+package com.unik.unikForma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikFormaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnikFormaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unik/unikForma/UnikFormaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unik.unikForma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnikFormaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
